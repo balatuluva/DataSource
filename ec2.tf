dc93b74d4a033516a290e9d6a676285faf02e131
balatuluva <bala.tulva@gmail.com>
2024-11-24 20:28:06 +0000

    data source config for remote state management correction

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web-1" {
   instance_type               = "t2.micro"
   key_name                    = "SecOps-Key"
   subnet_id                   = data.aws_subnet.Terraform_Public_Subnet1-testing.id
-  vpc_security_group_ids      = data.aws_security_group.allow_all.id
+  vpc_security_group_ids      = ["${data.aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
   tags                        = {
     Name       = "Server-1"
